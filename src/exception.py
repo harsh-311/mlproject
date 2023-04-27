95b272465fa5296afbde4b14cdb5163f318ec440
Harsh <harshp6065@gmail.com>
2023-04-27 23:41:14 +0530

    Eda And Problemstatement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
 import sys
 import logging
+# from logger import temone
+from src.logger import logging
+
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     filename=exc_tb.tb_frame.f_code.co_filename
@@ -12,14 +15,18 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
           super().__init__(error_message)
           self.error_message = error_message_details(error_message,error_detail=error_detail)
+        #   temone(self.error_message)
+          logging.info(self.error_message)
 
     def __str__(self):
          return self.error_message
     
 
-# if __name__ == "__main__":
-#     try:
-#          a=1/0
-#     except Exception as e:
-#         logging.info('Divide by Zero')
-#         raise CustomException(e,sys)
+if __name__ == "__main__": 
+    try:
+         a=1/0
+    except Exception as e:
+        # print(sys.path.append('PATH TO lo'),'heelo')
+        raise CustomException(e,sys)
+
+
