4a9f7bd25d598663a54acacf36efa56ba3937cd0
Harsh <harshp6065@gmail.com>
2023-05-03 11:17:51 +0530

    app and templetes file is done

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 import dill as pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
+from src.logger import logging
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -23,12 +23,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train,Y_train,x_test,y_test,models):
+def evaluate_models(X_train,Y_train,x_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(models.keys())):
             model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
+            GridSearch=GridSearchCV(model,param,cv=3)
+            GridSearch.fit(X_train,Y_train)
+
+            model.set_params(**GridSearch.best_params_)
+
+            # logging.info(f"Best Parameter is :{GridSearch.best_params_}")
+          
             model.fit(X_train,Y_train)
 
             y_prd=model.predict(x_test)
@@ -41,3 +49,12 @@ def evaluate_models(X_train,Y_train,x_test,y_test,models):
     
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
